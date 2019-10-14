6b84219f1126fa6ccc9ad86d4f8c00ccca669517
meghannhumanfactors <31716705+meghannhumanfactors@users.noreply.github.com>
2019-10-13 19:36:58 -0700

    This is the broken Update cs page

--- EnKindle/Pages/Update.cshtml.cs
+++ EnKindle/Pages/Update.cshtml.cs
@@ -4,37 +4,36 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Threading.Tasks;
 
 namespace EnKindle.Pages
 {
     public class UpdateModel : PageModel
 
     {
+        //SECTION 1 ... DECLARATIONS GO HERE AT THE TOP OF PAGE
         private DB _DB;
         private readonly ILogger _logger;
+
         public string Message { get; set; }
 
-        //////knows where to map your form... tells the onpost where to map your database table to update.... changed to _Greetings
+        //////TELL ON POST WHAT DATABASE TABLE TO UPDATE...knows where to map your form... tells the onpost where to map your database table to update.... changed to _Greetings
         [BindProperty]
         public Greetings _Greetings { get; set; }
 
-        //public UpdateModel(
-        //    DB dB,
-        //    ILogger<UpdateModel> logger
-        //    )
-        //{
-        //    _DB = dB;
-        //    _logger = logger;
-        //}
-
-        private IConfiguration _myConfiguration { get; set; }
-        public UpdateModel(DB myDB, IConfiguration myConfiguration)
+        //SECTION 2 ... CONSTRUCTORS 
+        //THIS IS THE CONSTRUCTOR. VERY IMPORTANT! 
+        public UpdateModel(
+            DB dB,
+            ILogger<UpdateModel> logger
+            )
         {
-            _DB = myDB;
-            _myConfiguration = myConfiguration;
-
+            _DB = dB;
+            _logger = logger;
         }
 
+ 
+       //SECTION 3...ONGET THIS HAPPENS BEFORE THE PAGE LOADS
         public IActionResult OnGet(int id = 0)
         {
             if (id > 0)
@@ -50,19 +49,41 @@ namespace EnKindle.Pages
 
         }
 
-        public IActionResult OnPost()
+        //SECTION 4... ONPOST, OCCURS WHEN PAGE IS SUBMIITED USUALLY BY A CLICK
+        //PATTERN BELOW:
+
+        // 1. GET THE RECORD FROM THE DATABASE TABLE YOU WANT TO UPDATE USING THE"[BindProperty]" DECLARED EARLIER IN SECTION 1 ABOVE.
+
+        // 2. ONCE YOU FIND THE RECORD, ASSIGN IT TO A TEMPORARY VARIABLE.
+
+        // 3. UPDATE THE TEMPORARY VARIABLE WITH THE FORM FIELDS USING THE "[BindProperty]" DECLARED EARLIER IN SECTION 1 ABOVE.
+
+        // 4. USE YOUR  "_DB TO UPATE THE DATABASE.
+
+        // 5. USE YOUR "_DB" TO SAVE THE CHANGES SUBMITTED FROM THE FORM.
+        
+        public async Task<IActionResult> OnPost()
         {
             try
-
             {
-                //UPDATE RECORD ON THE DB
-                _DB.Greetings.Update(_Greetings);
-                _DB.SaveChanges();
+                //Read pattern. This is (1) + (2) 
+                var tempGreetings = _DB.Greetings.Find(_Greetings.ID);
 
-                return RedirectToPage("Preview", new { id = _Greetings.ID });
+                if (ModelState.IsValid)
+                {
+                    //Read pattern above. This is (3)
+                    await TryUpdateModelAsync(tempGreetings, "_Greetings");
+
+                    //UPDATE RECORD ON THE DB
+                    _DB.Greetings.Update(_Greetings); // This is 4
+                    _DB.SaveChanges();  // This is 5
+
+                    return RedirectToPage("Preview", new { id = _Greetings.ID });
+                }
             }
             catch (Exception ex)
             {
+
                 _logger.LogInformation($"Update OnPost {ex.Message}", ex);
                 return Page();
             }
